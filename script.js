90a9cfb8a3e6a671bef3bafe73cef3d278d8a3cf
Aayush Seth <aayushseth8@gmail.com>
2021-07-17 13:04:59 -0700

    Add basic functionality to display books

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
 }
 
+function displayBooks() {
+  const container = document.querySelector('#container');
+  let element;
+  myLibrary.forEach(book => {
+    element = document.createElement('p');
+    element.textContent = `${book.title} by ${book.author}. It has ${book.pages} pages. Read: ${book.read}`;
+    container.appendChild(element);
+  });
+}
+
+addBookToLibrary("1984", "George Orwell", 325, false);
+addBookToLibrary("testbook", "authorname", 43, true);
+displayBooks();
