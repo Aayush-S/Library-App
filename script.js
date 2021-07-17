8130372abbd2250341bba5aebbb7478faf34b6b3
Aayush Seth <aayushseth8@gmail.com>
2021-07-17 13:00:05 -0700

    Create book constructor and function to add books to library array

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title,
+  this.author = author,
+  this.pages = pages,
+  this.read = read
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
 }
+
