606fa4fdede68ae8879b3897ed13a293463330c3
Aayush Seth <aayushseth8@gmail.com>
2021-07-17 16:12:11 -0700

    Create cards for displaying books and add styles

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function Book(title, author, pages, read) {
   this.author = author,
   this.pages = pages,
   this.read = read
+  displayed = false;
 }
 
 function addBookToLibrary(title, author, pages, read) {
@@ -13,14 +14,43 @@ function addBookToLibrary(title, author, pages, read) {
 
 function displayBooks() {
   const container = document.querySelector('#container');
-  let element;
+  let card;
   myLibrary.forEach(book => {
-    element = document.createElement('p');
-    element.textContent = `${book.title} by ${book.author}. It has ${book.pages} pages. Read: ${book.read}`;
-    container.appendChild(element);
+    if (!book.displayed) {
+      card = document.createElement('div');
+      card.classList.add('card')
+
+      let title = document.createElement('h3');
+      title.textContent = book.title;
+      card.appendChild(title);
+
+      let author = document.createElement('p');
+      author.textContent = book.author;
+      card.appendChild(author);
+
+      let pages = document.createElement('p');
+      pages.textContent = book.pages + " pages";
+      card.appendChild(pages);
+
+      let read = document.createElement('p');
+      read.textContent = book.read ? "Read" : "Not Read";
+      card.appendChild(read);
+
+      container.appendChild(card);
+      book.displayed = true;
+    }
   });
 }
 
-addBookToLibrary("1984", "George Orwell", 325, false);
+addBookToLibrary("1984", "George Orwell", 328, false);
 addBookToLibrary("testbook", "authorname", 43, true);
 displayBooks();
+
+addBookToLibrary("1984", "George Orwell", 328, false);
+addBookToLibrary("testbook", "authorname", 43, true);
+
+addBookToLibrary("1984", "George Orwell", 328, false);
+addBookToLibrary("testbook", "authorname", 43, true);
+
+displayBooks();
+
