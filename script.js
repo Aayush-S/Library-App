1761345c2f46bc2048e93a7ffc3d768f228923da
Aayush Seth <aayushseth8@gmail.com>
2021-07-17 23:49:22 -0700

    Create button to remove book

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function Book(title, author, pages, read) {
   this.author = author,
   this.pages = pages,
   this.read = read
-  displayed = false;
 }
 
 function addBookToLibrary(title, author, pages, read) {
@@ -13,34 +12,48 @@ function addBookToLibrary(title, author, pages, read) {
   displayBooks();
 }
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 function displayBooks() {
   const container = document.querySelector('#container');
+  removeAllChildNodes(container);
   let card;
-  myLibrary.forEach(book => {
-    if (!book.displayed) {
-      card = document.createElement('div');
-      card.classList.add('card')
+  myLibrary.forEach((book, index) => {
+    
+    card = document.createElement('div');
+    card.dataset.key = index;
+    card.classList.add('card');
 
-      let title = document.createElement('h3');
-      title.textContent = book.title;
-      card.appendChild(title);
+    let title = document.createElement('h3');
+    title.textContent = book.title;
+    card.appendChild(title);
 
-      let author = document.createElement('p');
-      author.textContent = book.author;
-      card.appendChild(author);
+    let author = document.createElement('p');
+    author.textContent = book.author;
+    card.appendChild(author);
 
-      let pages = document.createElement('p');
-      pages.textContent = book.pages + " pages";
-      card.appendChild(pages);
+    let pages = document.createElement('p');
+    pages.textContent = book.pages + " pages";
+    card.appendChild(pages);
 
-      let read = document.createElement('button');
-      read.classList.add('read-btn');
-      read.textContent = book.read ? "Read" : "Not Read";
-      card.appendChild(read);
+    let read = document.createElement('button');
+    read.classList.add('read-btn');
+    read.textContent = book.read ? "Read" : "Not Read";
+    card.appendChild(read);
 
-      container.appendChild(card);
-      book.displayed = true;
-    }
+    let remove = document.createElement('button');
+    remove.textContent = "Remove";
+    remove.addEventListener('click', function (e) {
+      let removed = myLibrary.splice(e.target.dataset.key, 1);
+      displayBooks();
+    });
+    card.appendChild(remove);
+
+    container.appendChild(card);
   });
 }
 
@@ -52,4 +65,26 @@ newBookBtn.addEventListener('click', () => {
     document.getElementById('book-pages').value,
     document.getElementById('book-read').checked);
 });
+
+
+addBookToLibrary('1', 'fds', 32, false);
+addBookToLibrary('2', 'fds', 32, false);
+addBookToLibrary('3', 'fds', 32, false);
+addBookToLibrary('4', 'fds', 32, false);
+addBookToLibrary('5', 'fds', 32, false);
+addBookToLibrary('6', 'fds', 32, false);
+
+
+// TODO: Use prototype stuff with this
+/*
+// TODO: Probably need to add the onlick as soon as the button is created.
+const readButtons = document.querySelectorAll(".read-btn");
+readButtons.forEach(button => button.addEventListener('click', () => {
+    console.log("change");
+    this.read = !this.read;
+    button.textContent = this.read ? "Read" : "Not Read";
+  })
+);
+*/
+
 
